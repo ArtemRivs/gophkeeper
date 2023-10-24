2c741adcbf41105e7538f27ba22e78358c9a122c
Artem Rivs <artem.rivs@gmail.com>
2023-10-24 16:33:36 +0100

    add register and login handlers

--- internal/server/handlers/grpc_handlers.go
+++ internal/server/handlers/grpc_handlers.go
@@ -2,6 +2,9 @@ package handlers
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"os"
 
 	pb "github.com/ArtemRivs/gophkeeper/internal/pkg/proto"
@@ -79,4 +82,46 @@ func CreateAuthUnaryInterceptor(storage storage.IRepository) func(ctx context.Co
 		ctx = sublogger.WithContext(ctx)
 		return handler(ctx, req)
 	}
+}
+
+func (s *Server) Register(ctx context.Context, in *pb.UserData) (*pb.LoginResult, error) {
+	logger := zerolog.Ctx(ctx)
+	logger.Info().Msg("Register request")
+	passwordHash := GetHashForClient(in)
+	statusCode := s.storage.AddClient(in.Login, passwordHash)
+	if statusCode.Code() == codes.AlreadyExists {
+		logger.Info().Msgf("A client with login %v already exists", in.Login)
+		return &pb.LoginResult{}, status.New(codes.AlreadyExists, "A client with this login already exists").Err()
+	}
+	if statusCode.Code() != codes.OK {
+		logger.Error().Err(statusCode.Err())
+		return &pb.LoginResult{}, statusCode.Err()
+	} else {
+		logger.Info().Msgf("A client with login %v successfully registered", in.Login)
+		return &pb.LoginResult{Token: passwordHash}, nil
+	}
+}
+
+func (s *Server) Login(ctx context.Context, in *pb.UserData) (*pb.LoginResult, error) {
+	logger := zerolog.Ctx(ctx)
+	logger.Info().Msg("Login request")
+	client, statusCode := s.storage.GetClientByLogin(in.Login)
+	if statusCode.Code() != codes.OK {
+		logger.Error().Err(statusCode.Err()).Msgf("Unable to retrieve client data from storage, error: %v", statusCode.Message())
+		return &pb.LoginResult{}, status.New(codes.NotFound, "The client with this login doesn't exist").Err()
+	}
+	passwordHash := GetHashForClient(in)
+	if passwordHash != client.PasswordHash {
+		logger.Info().Msgf("An incorrect password was received for client %v", in.Login)
+		return &pb.LoginResult{}, status.New(codes.InvalidArgument, "Incorrect password").Err()
+	}
+	logger.Info().Msgf("Client with login %v has been successfully authorized", in.Login)
+	return &pb.LoginResult{Token: passwordHash}, nil
+}
+
+func GetHashForClient(in *pb.UserData) string {
+	h := hmac.New(sha256.New, []byte(SecretKey))
+	h.Write([]byte(in.Password))
+	passwordHash := h.Sum(nil)
+	return hex.EncodeToString(passwordHash)
 }
