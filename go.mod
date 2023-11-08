4752a88204729d2fbdd7f2f2007dbf29db7dca9e
Artem Rivs <artem.rivs@gmail.com>
2023-11-08 16:29:44 +0000

    some fix #01

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/lib/pq v1.10.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
--- internal/server/db/db.go
+++ internal/server/db/db.go
@@ -2,11 +2,13 @@ package db
 
 import (
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 // RunMigrations - start migrations
 func RunMigrations(dbDSN string) error {
-	m, err := migrate.New("file://db/migrations", dbDSN)
+	m, err := migrate.New("file://internal/server/db/migrations", dbDSN)
 	if err != nil {
 		return err
 	}
